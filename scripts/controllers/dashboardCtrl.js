208afd6f4473a7f37ec0974ca7eb1d6c36ecc9e8
nikhilsangwan007 <nikhil.sangwan95@gmail.com>
2017-05-16 11:46:06 +0530

    add avg score and gpa

--- scripts/controllers/dashboardCtrl.js
+++ scripts/controllers/dashboardCtrl.js
@@ -12,6 +12,24 @@
 				}, function errorCallback(err) {
 					console.log('error occured', err);
 				});
-		});
+			
+			$scope.averageGPA = function(data) {
+				var sum = 0;
+				for (var i = 0; i < data.length; i++) {
+					sum += data[i].GPA;
+				}
+				// console.log(avg);
+				return sum/data.length;
+			}
 
+			$scope.averageScore = function(data) {
+				var sum = 0;
+				for (var i = 0; i < data.length; i++) {
+					sum += data[i].score;
+				}
+				// console.log(avg);
+				return sum/data.length;
+			}
+
+		});
 })();
