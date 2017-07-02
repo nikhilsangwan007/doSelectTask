eb67dfa5d324ee8171021bc1284d9e627833bf0f
Nikhil Sangwan <nikhil.sangwan95@gmail.com>
2017-07-02 23:35:58 +0530

    add node server

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ let express = require('express');
 let path = require('path');
 let app = express();
 
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 8000);
 
-app.get('/', function(req, res) {
-	// res.redirect('/');
+app.get('*', function(req, res) {
+	// res.redirect('index.html');
 	res.sendFile('index.html', {root: path.join(__dirname, './')})
 });
 
@@ -13,6 +13,6 @@ app.listen(app.get('port'), function(err) {
 	if (err) {
 		console.log('error occured', err);
 	}else {
-		console.log('magic is happening at PORT 8080');
+		console.log('magic is happening at PORT 8000');
 	}
 });
